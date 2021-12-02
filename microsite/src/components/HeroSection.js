4baeb640c2e9ddfdd29c4b8c8e1b98f832d43556
chuncaso90 <72043079+chuncaso90@users.noreply.github.com>
2021-12-01 23:08:06 -0800

    moved directory

--- microsite/src/components/HeroSection.js
+++ microsite/src/components/HeroSection.js
@@ -6,9 +6,9 @@ function HeroSection() {
   return (
     <div className='hero-container'>
       <video src='/videos/video-1.mp4' autoPlay loop muted />
-      <h1>Create a connection with a little liquid courage.</h1>
+      {/* <h1>Create a connection with a little liquid courage.</h1>
       <p>Vancouver is a lonely city, why not get help from alcohol?
-      </p>
+      </p> */}
     </div>
   );
 }
--- microsite/src/components/pages/progressreport.js
+++ microsite/src/components/pages/progressreport.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import '../../App.css';
+import '../pages/progressreport.css';
 import HeroSection from '../HeroSection';
+import styled from 'styled-components';
+import {FaArrowAltCircleRight, FaArrowAltCircleLeft,} from 'react-icons/fa'
+
+const Contprogress = styled.div`
+  height:1000px;
+  width: 500px;
+`;
+
 
 export default function Progress() {
   return (
-    <>
-      <HeroSection />
+    <div className='progresscont'>
+      <video src='/videos/video-1.mp4' autoPlay loop muted />
+      <div className='cont'>
+        <Contprogress>
 
-    </>
+        </Contprogress>
+
+         
+      </div>
+
+
+    </div>
   );
 }
